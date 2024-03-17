be31c0604cf814ecc281a9f9827f9de0e609e1cb
leiwingqueen <leiwingqueen@gmail.com>
2024-03-17 16:03:53 +0800

    implement block builder

--- table/block_builder.cc
+++ table/block_builder.cc
@@ -33,6 +33,7 @@
 
 #include "leveldb/comparator.h"
 #include "leveldb/options.h"
+
 #include "util/coding.h"
 
 namespace leveldb {
@@ -44,7 +45,10 @@ BlockBuilder::BlockBuilder(const Options* options)
 }
 
 void BlockBuilder::Reset() {
-  // TODO: implement BlockBuilder::Reset
+  // implement BlockBuilder::Reset
+  counter_ = 0;
+  restarts_.push_back(0);
+  finished_ = false;
 }
 
 size_t BlockBuilder::CurrentSizeEstimate() const {
@@ -54,13 +58,21 @@ size_t BlockBuilder::CurrentSizeEstimate() const {
 }
 
 Slice BlockBuilder::Finish() {
-  // TODO: implement BlockBuilder::Finish
-  // hint: append restart array to buffer_, then append restarts_.size() to buffer_
-  // each restart is a uint32_t, so you can use PutFixed32 to append each restart
-  // return a Slice of buffer_
+  // implement BlockBuilder::Finish
+  // hint: append restart array to buffer_, then append restarts_.size() to
+  // buffer_ each restart is a uint32_t, so you can use PutFixed32 to append
+  // each restart return a Slice of buffer_
   // - remember change finished_ to true
 
   // Append restart array
+  // The trailer of the block has the form:
+  //     restarts: uint32[num_restarts]
+  //     num_restarts: uint32
+  for (size_t i = 0; i < restarts_.size(); i++) {
+    PutFixed32(&buffer_, restarts_[i]);
+  }
+  PutFixed32(&buffer_, restarts_.size());
+  finished_ = true;
   return Slice(buffer_);
 }
 
@@ -70,7 +82,7 @@ void BlockBuilder::Add(const Slice& key, const Slice& value) {
   assert(counter_ <= options_->block_restart_interval);
   assert(buffer_.empty()  // No values yet?
          || options_->comparator->Compare(key, last_key_piece) > 0);
-  // TODO: implement BlockBuilder::Add
+  // implement BlockBuilder::Add
   // hint:
   // An entry for a particular key-value pair has the form:
   //     shared_bytes: varint32
@@ -80,6 +92,26 @@ void BlockBuilder::Add(const Slice& key, const Slice& value) {
   //     value: char[value_length]
   // shared_bytes == 0 for restart points.
   // remember to update last_key_ and counter_
+
+  size_t shared = 0;
+  int size = std::min(last_key_piece.size(), key.size());
+  while (shared < size && last_key_piece[shared] == key[shared]) {
+    shared++;
+  }
+  size_t unshared = key.size() - shared;
+  PutVarint32(&buffer_, shared);
+  PutVarint32(&buffer_, unshared);
+  PutVarint32(&buffer_, value.size());
+  buffer_.append(key.data() + unshared, unshared);
+  buffer_.append(value.data(), value.size());
+  // update last_key and counter
+  last_key_ = key.ToString();
+  counter_++;
+  if (counter_ >= options_->block_restart_interval) {
+    last_key_ = "";
+    counter_ = 0;
+    restarts_.push_back(buffer_.size());
+  }
 }
 
 }  // namespace leveldb
